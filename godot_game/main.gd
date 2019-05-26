f97c7391b8fb5c21e64e1d981b1bc38333140b00
Olga Kalinichenko <kalinich_1099649@spk-ssh-access1.if.uj.edu.pl>
2019-05-26 12:11:50 +0200

    Small fixes

--- godot_game/main.gd
+++ godot_game/main.gd
@@ -9,11 +9,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # delta czas ktory uplyna od poprzedniej klatki (0.01s)
 func _process(delta):
-	timer+=delta
+	timer += delta
 	
-	if timer > 1:
+
+	var width = ProjectSettings.get_setting("display/window/size/width")
+	
+	if timer > 4:
 		timer = 0.0
 		var new_object = crate_object.instance()
-		var i = randi()%500
+		var i = randi() % width
 		new_object.position = Vector2(i, -100)
 		$crates.add_child(new_object)
